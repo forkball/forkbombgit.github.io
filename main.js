a46bc85130274d01d6dd01ecb03b8dbf4fdc35bc
EROS <erosdipede@gmail.com>
2018-07-30 10:38:10 -0400

    Added small animations to switching project tasks

--- main.js
+++ main.js
@@ -1,17 +1,23 @@
 //handler for general nav
 $("#nav-general").on("click",function(e){
 	e.preventDefault();
-	$("#general").removeClass("hidden");
-	$("#games").addClass("hidden");
 	$(this).parent().addClass("active")
 	$("#nav-games").parent().removeClass("active");
+	$("#games").fadeOut(200,function(){
+		$("#general").removeClass("hidden");
+		$("#general").fadeIn(200);
+		$("#games").addClass("hidden");
+	});
 });
 
 //handler for game nav
 $("#nav-games").on("click",function(e){
 	e.preventDefault();
-	$("#games").removeClass("hidden");
-	$("#general").addClass("hidden");
 	$(this).parent().addClass("active")
 	$("#nav-general").parent().removeClass("active");
+	$("#general").fadeOut(200,function(){
+		$("#games").removeClass("hidden");
+		$("#games").fadeIn(200);
+		$("#general").addClass("hidden");
+	});
 });
