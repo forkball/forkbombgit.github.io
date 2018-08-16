dad1db893996fc73716f7618e52880ff2eee31db
Eros <erosdipede@cmail.carleton.ca>
2018-08-16 11:16:37 -0400

    added panels to page content

--- main.js
+++ main.js
@@ -1,23 +1,23 @@
 //handler for general nav
-$("#nav-general").on("click",function(e){
-	e.preventDefault();
-	$(this).parent().addClass("active")
-	$("#nav-games").parent().removeClass("active");
-	$("#games").fadeOut(200,function(){
-		$("#general").removeClass("hidden");
-		$("#general").fadeIn(200);
-		$("#games").addClass("hidden");
-	});
+$("#nav-general").on("click", function(e) {
+  e.preventDefault();
+  $(this).parent().addClass("active")
+  $("#nav-games").parent().removeClass("active");
+  $("#games").fadeOut(200, function() {
+    $("#general").removeClass("hidden");
+    $("#general").fadeIn(200);
+    $("#games").addClass("hidden");
+  });
 });
 
 //handler for game nav
-$("#nav-games").on("click",function(e){
-	e.preventDefault();
-	$(this).parent().addClass("active")
-	$("#nav-general").parent().removeClass("active");
-	$("#general").fadeOut(200,function(){
-		$("#games").removeClass("hidden");
-		$("#games").fadeIn(200);
-		$("#general").addClass("hidden");
-	});
+$("#nav-games").on("click", function(e) {
+  e.preventDefault();
+  $(this).parent().addClass("active")
+  $("#nav-general").parent().removeClass("active");
+  $("#general").fadeOut(200, function() {
+    $("#games").removeClass("hidden");
+    $("#games").fadeIn(200);
+    $("#general").addClass("hidden");
+  });
 });
