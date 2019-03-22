3b486d76cb9fb829e181f217c0d92ebd566757b1
ForkBombGIT <erosdipede@gmail.com>
2019-03-21 23:10:46 -0400

    Update to single panel than 2, add side navigation

--- scripts/main.js
+++ scripts/main.js
@@ -1,64 +1,32 @@
-$(".ml6 .letters").each(function() {
-  $(this).html(
-    $(this)
-      .text()
-      .replace(/([^\x00-\x80]|\w)/g, "<span class='letter'>$&</span>")
-  );
-});
-
-var bobble = function() {
-  anime
-    .timeline({ loop: false })
-    .add({
-      targets: ".ml6 .letter",
-      translateY: ["1.1em", 0],
-      duration: 750,
-      delay: function(el, i) {
-        return i;
-      }
-    })
-    .add({
-      targets: ".ml6",
-      opacity: 1,
-      duration: 1000,
-      easing: "easeOutExpo",
-      delay: 1000
-    });
-};
-
-bobble();
-$("h1 > span").on("click", function(e) {
-  bobble();
+//handler for general nav
+$("#nav-about").on("click", function(e) {
+  e.preventDefault();
+  $(this)
+    .addClass("active");
+  $("#nav-projects,#nav-games")
+    .removeClass("active");
+  $("#about").removeClass("hidden");
+  $("#general,#games").addClass("hidden");
 });
 
 //handler for general nav
-$("#nav-general").on("click", function(e) {
+$("#nav-projects").on("click", function(e) {
   e.preventDefault();
   $(this)
-    .parent()
     .addClass("active");
-  $("#nav-games")
-    .parent()
+  $("#nav-about,#nav-games")
     .removeClass("active");
-  $("#games").fadeOut(200, function() {
-    $("#general").removeClass("hidden");
-    $("#general").fadeIn(200);
-    $("#games").addClass("hidden");
-  });
+  $("#general").removeClass("hidden");
+  $("#about,#games").addClass("hidden");
 });
 
-//handler for game nav
+//handler for general nav
 $("#nav-games").on("click", function(e) {
   e.preventDefault();
   $(this)
-    .parent()
     .addClass("active");
-  $("#nav-general")
-    .parent()
+  $("#nav-about,#nav-projects")
     .removeClass("active");
-  $("#general").fadeOut(200, function() {
-    $("#games").removeClass("hidden");
-    $("#games").fadeIn(200);
-    $("#general").addClass("hidden");
-  });
+  $("#games").removeClass("hidden");
+  $("#general,#about").addClass("hidden");
 });
