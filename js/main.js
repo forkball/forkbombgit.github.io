9b9bed880822845cd64662c6f469be62b055a6f2
ForkBombGIT <erosdipede@gmail.com>
2019-06-18 17:32:16 -0700

    Add keyboard navigation

--- js/main.js
+++ js/main.js
@@ -1,9 +1,46 @@
 window.onload = function() {
-    //nav event handlers
+    //nav event handlers, clicking
     document.getElementById("nav-about").addEventListener("click", function() { handleNavigationClick("nav-about") });
     document.getElementById("nav-work").addEventListener("click", function() { handleNavigationClick("nav-work") });
     document.getElementById("nav-projects").addEventListener("click", function() { handleNavigationClick("nav-projects") });
     document.getElementById("nav-games").addEventListener("click", function() { handleNavigationClick("nav-games") });
+    document.getElementById("nav-contact").addEventListener("click", function() { handleNavigationClick("nav-contact") });
+
+    //nav event handlers, enter
+    document.getElementById("nav-about").addEventListener("keyup", function(event) {
+        event.preventDefault();
+        if (event.keyCode === 13) {
+            document.getElementById("nav-about").click();
+        }
+    });
+
+    document.getElementById("nav-work").addEventListener("keyup", function(event) {
+        event.preventDefault();
+        if (event.keyCode === 13) {
+            document.getElementById("nav-work").click();
+        }
+    });
+
+    document.getElementById("nav-projects").addEventListener("keyup", function(event) {
+        event.preventDefault();
+        if (event.keyCode === 13) {
+            document.getElementById("nav-projects").click();
+        }
+    });
+
+    document.getElementById("nav-games").addEventListener("keyup", function(event) {
+        event.preventDefault();
+        if (event.keyCode === 13) {
+            document.getElementById("nav-games").click();
+        }
+    });
+
+    document.getElementById("nav-contact").addEventListener("keyup", function(event) {
+        event.preventDefault();
+        if (event.keyCode === 13) {
+            document.getElementById("nav-contact").click();
+        }
+    });
 };
 
 function handleNavigationClick(nav) {
