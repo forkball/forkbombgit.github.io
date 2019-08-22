9060e8d6c7dd97acbb2a161d46ec1cee2dbb668b
ForkBombGIT <erosdipede@gmail.com>
2019-08-22 13:37:53 -0400

    Update content, Fix some accessibility issues

--- js/main.js
+++ js/main.js
@@ -56,24 +56,31 @@ window.onload = function() {
 
 function handleNavigationClick(nav) {
     //nav management
-    var activeNav = document.getElementsByClassName("is-active")[0]
+    var activeNav = document.getElementsByClassName("is-active")[0];
     if (nav !== activeNav.children[0].id) {
         activeNav.classList.remove("is-active");
         document.getElementById(nav).parentElement.classList.add("is-active");
 
         //content management
-        document.getElementById(nav.replace("nav", "page")).removeAttribute("hidden");
-        document.getElementById((activeNav.children[0].id).replace("nav", "page")).setAttribute("hidden", "true");
+        document
+            .getElementById(nav.replace("nav", "page"))
+            .removeAttribute("hidden");
+        document
+            .getElementById(activeNav.children[0].id.replace("nav", "page"))
+            .setAttribute("hidden", "true");
     }
-};
+}
 
 function navSelectChange() {
     var select = document.getElementById("dropdown-nav");
-    var selectValue = select.options[select.selectedIndex].value.replace("nav", "page");
+    var selectValue = select.options[select.selectedIndex].value.replace(
+        "nav",
+        "page"
+    );
 
     //content management
     document.getElementById(selectValue).removeAttribute("hidden");
-    document.querySelectorAll('[id^="page-"]').forEach((e) => {
+    document.querySelectorAll('[id^="page-"]').forEach(e => {
         if (e.id !== selectValue) {
             e.setAttribute("hidden", "true");
         }
