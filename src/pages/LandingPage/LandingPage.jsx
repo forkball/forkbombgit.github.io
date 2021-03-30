c036cd05c8ae7a9391b83e9edbc4b8391922d690
ForkBombGIT <erosdipede@gmail.com>
2021-03-30 13:06:25 -0400

    Reinitialize project with CRA
    - Add project structure directories
    - Initialize landing page
    - Add tailwind
    - Add prettier and eslint

---
+++ src/pages/LandingPage/LandingPage.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import './LandingPage.scss';
+
+function LandingPage() {
+  return (
+    <div className="landing">
+      <div className="name container p-4 mx-auto m-">
+        <h1>
+          <span className="first-name font-bold">EROS</span> DI PEDE
+        </h1>
+      </div>
+    </div>
+  );
+}
+
+export default LandingPage;
