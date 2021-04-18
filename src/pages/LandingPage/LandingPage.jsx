8cd8ebcea366ce6cafdf5cf5e7878aee09b06ca5
ForkBombGIT <erosdipede@gmail.com>
2021-04-17 20:16:27 -0400

    Add LandingPage headers

--- src/pages/LandingPage/LandingPage.jsx
+++ src/pages/LandingPage/LandingPage.jsx
@@ -4,10 +4,24 @@ import './LandingPage.scss';
 function LandingPage() {
   return (
     <div className="landing">
-      <div className="name container p-4 mx-auto m-">
-        <h1>
+      <div className="landing-panel landing-panel-1 container p-4 mx-auto">
+        <h1 id="name">
           <span className="first-name font-bold">EROS</span> DI PEDE
         </h1>
+      </div>
+      <div className="landing-panel landing-panel-1 container p-4 mx-auto">
+        <div id="landing-image" />
+        <h2 id="specialty">
+          <span>GAME</span>
+          and
+          <span>WEB</span>
+        </h2>
+        <h2 id="role">
+          <strong>DEVELOPER</strong>
+        </h2>
+      </div>
+      <div className="landing-panel landing-panel-2 container absolute p-4 w-48 m-10 h-1/2 top-0 right-0 mr-36 z-10 text-right">
+        <h3>LANGUAGES</h3>
       </div>
     </div>
   );
