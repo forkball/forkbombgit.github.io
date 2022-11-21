7018030dc3d21b0287d6162f7b54f774427e33e2
Eros Di Pede <erosdipede@gmail.com>
2022-11-21 09:22:00 -0500

    Add background to h1

--- src/pages/LandingPage/LandingPage.js
+++ src/pages/LandingPage/LandingPage.js
@@ -7,7 +7,7 @@ function LandingPage() {
     <div className="flex flex-col justify-center items-center py-4 h-screen">
       <Device classes="tablet:block hidden" />
       <p className="font-pixel tablet:hidden block">under construction...</p>
-      <h1 className="mr-auto absolute bottom-2 left-2">forkball.games</h1>
+      <h1 className="mr-auto absolute bottom-0 left-0">forkball.games</h1>
     </div>
   );
 }
